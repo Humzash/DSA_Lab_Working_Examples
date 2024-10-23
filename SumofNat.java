786ea7546ddc30cab449925ab3fbedb2e3cfb3ca
Humzash <humzash05@gmail.com>
2024-10-23 09:35:28 +0500

    Add files via upload

---
+++ SumofNat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.task01;
+/**
+ *
+ * @author 123
+ */
+public class SumofNat {
+    public static void main(String[] args) {
+       int n = 10;
+       int sum = n * (n+1)/2;
+       System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+    }
+            
+}
